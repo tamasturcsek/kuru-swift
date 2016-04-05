3cbf3475c4e2ea39248732a31524bae1abd55c1a
Tamas Turcsek <tamasturcsek@Tamass-Mac-mini.local>
2016-04-05 19:50:13 +0200

    init

---
+++ kuru-swift/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  kuru-swift
+//
+//  Created by Tamas Turcsek on 05/04/16.
+//  Copyright © 2016 Meruem Software. All rights reserved.
+//
+
+import Foundation
---
+++ kuru-swift/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  kuru-swift
+//
+//  Created by Tamas Turcsek on 05/04/16.
+//  Copyright © 2016 Meruem Software. All rights reserved.
+//
+
+import Foundation
